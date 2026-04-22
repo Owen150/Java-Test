87edb8fd29601286b8161354c3dc3f0a0c1ecbf5
Owen150 <michaeloduor50@gmail.com>
2026-04-23 02:02:13 +0300

    Updates

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        int age = 30;
-        System.out.println(age);
+        //Primitive Types - 8
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+
+        //Variables are stored in different memory locations, Independent of each other.
+        //Primitive Types Store Actual Values.
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);  //Remains Unchanged
+
+        //Reference Types do not store actual values, but a reference to an object somewhere in the memory.
+        Date now = new Date();
+        System.out.println(now);
+
+        String message = "Hello World" + "!!";
+        System.out.println(message);
+
+        Scanner scanner = new Scanner(System.in);
+        //byte ages = scanner.nextByte(System.out.println("Enter Your Age: "));
     }
 }
