61f18154c19c362c45819cad691c5d661c1dc56c
Owen150 <michaeloduor50@gmail.com>
2026-05-17 03:52:22 +0300

    Code Restructure

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         /*
-        Variables
-        A. Using a Coma, we can declare multiple variables on the same line.
-        int age = 12, temperature = 20;
+        1. Variables
+            A. Using a Coma, we can declare multiple variables on the same line.
+            int age = 12, temperature = 20;
 
         B. We can copy the value of one variable into another.
-        int myAge = 27;
-        int herAge = myAge;
+            int myAge = 27;
+            int herAge = myAge;
 
-        1. Primitive Types
+        i. Primitive Types
         Variables are stored in different memory locations, Independent of each other.
         Store Actual Values.
         8 in Total: Byte, short, int, long, float, double, char, boolean.
@@ -40,47 +40,47 @@ public class Main {
         x = 2;
         System.out.println(y);  //Value of y remains unchanged .i.e y = 1.
 
-        2. Reference Types
+        ii. Reference Types
         Variables do not store actual values but a reference to an object located somewhere in the memory.
         Stores the Memory Address Location.
 
-        Date Object:
-        Date now = new Date();
-        System.out.println(now); //Displays the current date & time.
+            A. Date Object
+                Date now = new Date();
+                System.out.println(now); //Displays the current date & time.
 
-        Pointer Object:
-        Point point1 = new Point(1,2);
-        Point point2 = point1;
-        point1.x = 3;
-        System.out.println(point2); //Pointer value is updated .i.e. {3,2}
+            B. Pointer Object
+                Point point1 = new Point(1,2);
+                Point point2 = point1;
+                point1.x = 3;
+                System.out.println(point2); //Pointer value is updated .i.e. {3,2}
 
-        3. Strings
+        2. Strings
         String objects are immutable/Unchangeable .i.e. Creates a new instance of the string object.
 
         String message = "Hello World" + "!!";
         System.out.println(message.indexOf("e"));
         message.length, .startsWith(), .endsWith(), .replace(target, replacement), .trim() - removes whitespaces, .toUpperCase(), .toLowerCase() etc.
 
-        4. Arrays
+        3. Arrays
+            i. Single Arrays
+                int[] numbers = new int[5];
+                numbers[0] = 1;
+                numbers[1] = 2;
+                System.out.println(Arrays.toString(numbers));
 
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        System.out.println(Arrays.toString(numbers));
+                int[] points = {2, 3, 6, 1, 2};
+                Arrays.sort(points);
+                System.out.println(Arrays.toString(points));
 
-        int[] points = {2, 3, 6, 1, 2};
-        Arrays.sort(points);
-        System.out.println(Arrays.toString(points));
-
-        5. Multi-Dimensional Arrays
-
-        Two Rows and Three Columns.
-        int[][] multi = new int[2][3]; //also, int[][] multi = {{2,3,6},{1,2,3}};
-        multi[0][0] = 1; //First Row, First Column
-        System.out.println(Arrays.deepToString(multi)); //deepToString() for printing Multi-dimensional arrays.
-        Output - Rows printed in Horizontal Order, Columns printed Vertically, both separated by a coma/s .i.e. [[1, 0, 0], [0, 0, 0]].
+            ii. Multi-Dimensional Arrays
+                Two Rows and Three Columns.
+                int[][] multi = new int[2][3]; //also, int[][] multi = {{2,3,6},{1,2,3}};
+                multi[0][0] = 1; //First Row, First Column
+                System.out.println(Arrays.deepToString(multi)); //deepToString() method for printing Multi-dimensional arrays.
+                Output - Rows printed in Horizontal Order, Columns printed Vertically, both separated by a coma/s .i.e. [[1, 0, 0], [0, 0, 0]].
 
         6. Arithmetic Operations
+        Operands: +, -, *, /, %
         int z = 1;
         z++;
         System.out.println(z); //2
