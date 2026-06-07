ce25dfddf6e4daba63510b40e499c6d52b8ba8ce
Owen150 <michaeloduor50@gmail.com>
2026-06-08 01:04:20 +0300

    Highway

---
+++ src/Highway.java
@@ -0,0 +1,7 @@
+public class Highway {
+    public static void main(String[] args){
+        Car fordMustang = new Car();
+        SelfDrivingCar kitt = new SelfDrivingCar();
+        fordMustang.applyBrakes();
+    }
+}
