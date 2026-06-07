cee611c514e4439b8c9a45d3f2cd39f76e12f44c
Owen150 <michaeloduor50@gmail.com>
2026-06-07 23:56:14 +0300

    Car Class

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    public void applyBrakes(){
+        System.out.println("Driver Applies Brakes");
+        checkSurroundings();
+    }
+
+    public void checkSurroundings(){
+        System.out.println("Driver Checks Surroundings");
+    }
+}
