788c0f16eed494e191bfa84c4fe9e3487a4da732
Owen150 <michaeloduor50@gmail.com>
2026-06-16 20:50:32 +0300

    Accessing Private Variables - Getter and Setter Methods

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    private int numOfLegs = 4;
+
+    public int getNumOfLegs() {
+        return numOfLegs;
+    }
+
+    public void setNumOfLegs(int numOfLegs) {
+        this.numOfLegs = numOfLegs;
+    }
+}
