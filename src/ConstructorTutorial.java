4310c9e44bc239995dd69fe49403a27ef7f4971c
Owen150 <michaeloduor50@gmail.com>
2026-06-24 21:13:23 +0300

    Constructors + Static Methods and Variables

--- src/ConstructorTutorial.java
+++ src/ConstructorTutorial.java
@@ -2,7 +2,7 @@ public class ConstructorTutorial {
     private static int constructorCount = 0; //Static Variable - Shared among all objects of a class. Memory Efficiency (Don't need to redeclare similar variables) + Counters/Constants.
     private String constructorName;
 
-    //Constructor - Sets up and constructs the data in a new object after creation/initialization.
+    //Constructor - Sets up and constructs the data in a new object after creation/initialization. If a class has no constructors Java will create a no-parameter constructor.
     //A Constructor behaves much like a non-static method i.e. Cannot be called without using an individual object as a reference.
     public ConstructorTutorial(){
         //Every time we create a new object constructorCount increases by 1. We can use the value of constructorCount++ to keep track of how many ConstructorTutorial objects have been created.
@@ -26,10 +26,10 @@ public class ConstructorTutorial {
 
     public void getConstructorName() {
         System.out.println(constructorName);
-        //System.out.println(constructorCount);
     }
 
-    //Static Method - Can be called without using an individual object as a reference. Usage - Accessing Static Data i.e. constructorCount
+    //Static Method - Can be called without using an individual object as a reference.
+    // Usage - 1. Accessing Static Data i.e. constructorCount, 2. Utility/Helper Functions - Performing operations that don't depend on an object's state e.g. Mathematical Calculations, 3. Factory Methods - Creating instances of a class or performing setup tasks independent of a specific object.
     public static void getConstructorCount() {
         System.out.println(constructorCount);
     }
