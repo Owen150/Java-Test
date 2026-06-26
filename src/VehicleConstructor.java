d9d2adaef8074d5ffc2bb3c3bf2667866df4934a
Owen150 <michaeloduor50@gmail.com>
2026-06-26 23:56:41 +0300

    Calling Constructors

--- src/VehicleConstructor.java
+++ src/VehicleConstructor.java
@@ -3,16 +3,16 @@ public class VehicleConstructor {
     private int vin;
 
     public VehicleConstructor(){
-        super();
+        super(); //Call to the Java Object class. Constructs a New Object.
     }
 
     public VehicleConstructor(String newColor){
-        this();
+        this(); //Call to the same VehicleConstructor inside the same class but with no parameters i.e.VehicleConstructor() which calls super i.e. The Java Object class
         color = newColor;
     }
 
     public VehicleConstructor(String newColor, int newVin){
-        super();
+        super(); //Call to the Java Object Class. Constructs a new Object.
         color = newColor;
         vin = newVin;
     }
