36b593f1b5a45ae08ba6f6e3944147d6c8b03c77
Owen150 <michaeloduor50@gmail.com>
2026-06-27 01:42:05 +0300

    Passing Data to a Superclass Constructor

--- src/GhostConstructor.java
+++ src/GhostConstructor.java
@@ -1,3 +1,10 @@
 public class GhostConstructor {
     //Passing Data to a Superclass Constructor
+    private String color;
+    private boolean isScary;
+
+    public  GhostConstructor(String color, boolean isScary) {
+        this.color = color;
+        this.isScary = isScary;
+    }
 }
