1de3d154650cd24eda075c5ee3cc2ce4265dacd7
Owen150 <michaeloduor50@gmail.com>
2026-06-27 19:49:28 +0300

    Superclass Constructor

--- src/Slimer.java
+++ src/Slimer.java
@@ -1,9 +1,9 @@
 public class Slimer extends GhostConstructor{
     private boolean isHungry;
 
-    //Passing data to a Superclss Constructor.
+    //Passing data to a Superclass Constructor i.e. GhostConstructor
     public Slimer(boolean isHungry, boolean isScary) {
-        super("Green", isScary);
+        super("Green", isScary); //Creates a New GhostConstructor Object
         this.isHungry = isHungry;
     }
 }
